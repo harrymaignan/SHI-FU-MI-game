cb2beb068d3bcaff3ea71800cf5a027ace8d2e75
Djola <djolakeita21@gmail.com>
2026-04-15 11:41:00 +0200

    choix du robot

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function ChoixJoueur(choix) {
 Pierre.addEventListener("click", () => ChoixJoueur(pierre));
 Ciseaux.addEventListener("click", () => ChoixJoueur(ciseaux));
 Feuille.addEventListener("click", () => ChoixJoueur(feuille));
+
+
+function botChoice() {
+    const choices = ['pierre', 'feuille', 'ciseaux'];
+    const noteRandom = Math.floor(Math.random() * choices.length);
+    return choices[noteRandom];
+}
+console.log("Le bot a choisi : " + (botChoice()));
+
+function choices(choicePlayer, choiceBot) {
+
+    if (choicePlayer === choiceBot) {
+        return "égalité";
+    } 
+}
